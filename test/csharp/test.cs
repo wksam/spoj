806a875e39dd670041edee2ac34f58a718138273
Sam <wk.sams@gmail.com>
2020-04-24 11:37:43 -0300

    add bash and c

--- test/csharp/test.cs
+++ test/csharp/test.cs
@@ -2,10 +2,9 @@ using System;
 
 public class Test {
     public static void Main() {
-        string line;
-        while((line = Console.ReadLine()) != null) {
-            if(line == "42") break;
-            Console.WriteLine(line);
+        string number;
+        while((number = int.Parse(Console.ReadLine())) != 42) {
+            Console.WriteLine(number);
         }
     }
 }
