b3d32753c9ff9b86ade8d48e96a319831a055669
Sam <wk.sams@gmail.com>
2020-04-24 11:55:54 -0300

    add kotlin

---
+++ test/kotlin/test.kt
@@ -0,0 +1,10 @@
+import java.util.*
+ 
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    while(true) {
+    	val n = sc.nextInt();
+    	if(n == 42) break;
+    	println(n)
+    }
+}
