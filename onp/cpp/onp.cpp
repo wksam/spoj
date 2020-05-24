02d76d48c7038f20b6da9a1cff0cfd6d2ff4dd6e
Sam <wk.sams@gmail.com>
2020-05-24 13:23:58 -0300

    add onp cpp - wrong answer

---
+++ onp/cpp/onp.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <stack>
+#include <ctype.h>
+using namespace std;
+
+int main() {
+	int testCase = 0;
+	cin >> testCase;
+	for (int t = 0; t < testCase; t++){
+		stack <char> s;
+		string rpn = "";
+		
+		string expression = "";
+		cin >> expression;
+		
+		for (char symbol : expression) {
+			if (isalpha(symbol)) {
+				rpn += symbol;
+			} else {
+				if (symbol == ')') {
+					bool found = false;
+					while (!found) {
+						rpn += s.top() != '(' ? s.top() : '\0';
+						found = s.top() == '(';
+						s.pop();
+					}
+				} else if (symbol == '^') {
+					bool stopPop = false;
+					while (!stopPop) {
+						if (s.top() == '+' || s.top() == '-' || s.top() == '*' || s.top() == '/' || s.top() == '^') {
+							rpn += s.top();
+							s.pop();
+						} else {
+							s.push(symbol);
+							stopPop = true;
+						}
+					}
+				} else if (symbol == '*' || symbol == '/') {
+					bool stopPop = false;
+					while (!stopPop) {
+						if (s.top() == '+' || s.top() == '-' || s.top() == '*' || s.top() == '/') {
+							rpn += s.top();
+							s.pop();
+						} else {
+							s.push(symbol);
+							stopPop = true;
+						}
+					}
+				} else if (symbol == '+' || symbol == '-') {
+					bool stopPop = false;
+					while (!stopPop) {
+						if (s.top() == '+' || s.top() == '-') {
+							rpn += s.top();
+							s.pop();
+						} else {
+							s.push(symbol);
+							stopPop = true;
+						}
+					}
+				} else {
+					s.push(symbol);
+				}
+			}
+		}
+        cout << rpn << endl;
+    }
+	return 0;
+}
